f9fd2856650162fae0a2adaf385536fcd4eaeda1
fayzalia23 <106764263+fayzalia23@users.noreply.github.com>
2022-10-25 10:21:13 +0700

    terbaru fayza

--- function.php
+++ function.php
@@ -21,6 +21,14 @@ function getMatkul($smt){
     return $query;
 }
 
+// get matkul with kode_mk
+function getMatkulDetail($kode_mk){
+    global $conn;
+    $query = mysqli_query($conn, "SELECT * FROM tb_matakul WHERE kode_mk = '$kode_mk'");
+    $data = mysqli_fetch_assoc($query);
+    return $data;
+}
+
 //get detail irs
 function getIrsDetail($nim){
     global $conn;
@@ -60,4 +68,39 @@ function getSkripsiDetail($nim){
     return $data;
 }
 
-?>
+//get detail irs diambil
+function getIrsDiambil($nim){
+    global $conn;
+    $query = mysqli_query($conn, "SELECT * FROM tb_irs_diambil WHERE nim='$nim'");
+    return $query;
+}
+
+//check irs diambil with kode_mk and nim
+function checkIrsDiambil($kode_mk, $nim){
+    global $conn;
+    $query = mysqli_query($conn, "SELECT * FROM tb_irs_diambil WHERE kode_mk='$kode_mk' AND nim='$nim'");
+    $data = mysqli_num_rows($query);
+    return $data;
+}
+
+// add data to irs diambil
+function addIrsDiambil($nim, $kode_mk){
+    $matkulDetail = getMatkulDetail($kode_mk);
+
+    $matakuliah = $matkulDetail['matakuliah'];
+    $waktu = $matkulDetail['waktu'];
+    $sks = $matkulDetail['sks'];
+    $kelas = $matkulDetail['kelas'];
+    $pembelajaran = $matkulDetail['pembelajaran'];
+
+    global $conn;
+    $query = mysqli_query($conn, "INSERT INTO tb_irs_diambil VALUES ('$nim', '$kode_mk', '$matakuliah', '$waktu', '$sks', '$kelas', '$pembelajaran', 'Belum Disetujui')");
+    return $query;
+}
+
+// delete data from irs diambil
+function deleteIrsDiambil($nim, $kode_mk){
+    global $conn;
+    $query = mysqli_query($conn, "DELETE FROM tb_irs_diambil WHERE nim='$nim' AND kode_mk='$kode_mk'");
+    return $query;
+}
